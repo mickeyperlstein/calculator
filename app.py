a2b1d880036743531bfb1dce6f6b0791b486c311
Mickey Perlstein <corp.mp@gmail.com>
2024-04-30 17:03:24 +0300

    final touches

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ from math import pow
 from typing import Callable
 import numbers
 from py_expression_eval import Parser
-from math_eval import operator
-class Calculation(BaseModel):
-    operator: str
-    operand1: float
-    operand2: float
+from math_eval import safe_compute, precedence_map, compute, binops, safe_binops
+import operator
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 class CalculationString(BaseModel):
     expression: str
+
+    def __repr__(self):
+        return f"{self.expression}"
 
 class Operator:
     def __init__(self, symbol: str, implementation: Callable[[float, float], float], priority: int):
@@ -21,12 +24,26 @@ class Operator:
         self.priority = priority
 
 class OperatorFactory:
-    def __init__(self):
-        Parser
-        self.operators = {}
-    
+        
     def register_operator(self, operator: Operator):
-        self.operators[operator.symbol] = operator
+            name = operator.symbol
+            # binops is the name of operations that have (a,b) params
+            # safe_binops is a special case that uses safe_compute,
+            # i decided to use compute with safe=false, so i am using binops accordingly
+            if name in binops.keys(): 
+                function = binops[name]
+                del binops[name]
+
+                # the search for precendence method searches by token and attaches a function to the token
+                # so its very important not to have more than one implementation of the same thing
+                # in the module ^ is bitwiseXOR so this inteferes with power which it implements under token **
+                # as the request was for ^ to map to power, this needs to be cleared correcly to work
+                if function in precedence_map.keys():
+                    del precedence_map[function] # Without this, its impossible to replace existing functions with custom ones
+
+            function = binops[name] = operator.implementation
+            precedence_map[function] = operator.priority
+        
     
     def get_operator(self, symbol: str) -> Operator:
         if symbol not in self.operators:
@@ -36,143 +53,77 @@ class OperatorFactory:
 operator_factory = OperatorFactory()
 
 def register_default_operators():
-    operator_factory.register_operator(Operator('+', lambda a, b: a + b, 1))
-    operator_factory.register_operator(Operator('-', lambda a, b: a - b, 1))
-    operator_factory.register_operator(Operator('*', lambda a, b: a * b, 2))
-    operator_factory.register_operator(Operator('^', lambda a, b: pow(a, b), 3))
-    operator_factory.register_operator(Operator('/', lambda a, b: a / b if b != 0 else "Error: Division by zero", 2))
 
-register_default_operators()
+    # the module i used is much more advanced and has many more operators,
+    # to show how i have actually completely stripped it down, i am clearing it's builtin abilities
+    # i could ofcourse let them be, and it would work just fine
+    binops.clear()
+    precedence_map.clear()
+
+    def my_pow(a,b):
+        return pow(a,b)
+    
+    operator_factory.register_operator(Operator('^', my_pow, 5))
+    
+    def my_sum(a,b):
+        return a+b
+    
+    operator_factory.register_operator(Operator('+', my_sum, 2))
+    def my_sub(a,b):
+        return a-b
+    operator_factory.register_operator(Operator('-', my_sub, 2))
+    
+    def my_mul(a,b):
+        return a*b
+    operator_factory.register_operator(Operator('*', my_mul, 3))
+    
+    def my_div(a,b):
+         return a / b if b != 0 else "Error: Division by zero"
+
+    operator_factory.register_operator(Operator('/', my_div, 3))
+    
+    
+
 
 def calculate(operator: str, operand1: float, operand2: float) -> float:
-    op = operator_factory.get_operator(operator)
-    return op.implementation(operand1, operand2)
+    try:
+        op = operator_factory.get_operator(operator)
+        return op.implementation(operand1, operand2)
+    except KeyError as key:
+        raise ValueError(f"Invalid operator: {key}")
 
-# class Node:
-#     def __init__(self,value,left,right):
-#         self.left = left
-#         self.right = right
-#         self.value = value
-
-#     def __repr__(self):
-#         return f"{
-#                 'operator': {self.value},
-#                 'left': {self.left},
-#                 'right': {self.right}
-#             }"
-
-
-# import re
-
-# def create_tree(expression):
-#     stack = []
-#     tokens = re.findall(r"[-+*/^]|\d+(?:\.\d+)?|[A-Z]+", expression)
-    
-#     # Handle exponents
-#     pow_idx = tokens.index("^")
-#     while pow_idx > -1:
-#         numB4 = tokens[pow_idx-1]
-#         numafter = tokens[pow_idx+1]
-#         op  = tokens[pow_idx]
-#         tokens[pow_idx-1] = tokens[pow_idx] = tokens[pow_idx+1] = None
-#         stack.append({
-#                 'operator': op,
-#                 'left': numB4,
-#                 'right': numafter
-#             })
-#         try:
-#             pow_idx = tokens.index("^")
-#         except ValueError:
-#             break
-    
-#     # Handle multiplication and division
-#     mul_div_idx = next((i for i, x in enumerate(tokens) if x in ['*', '/']), -1)
-#     while mul_div_idx > -1:
-#         numB4 = tokens[mul_div_idx-1]
-#         numafter = tokens[mul_div_idx+1]
-#         op  = tokens[mul_div_idx]
-#         tokens[mul_div_idx-1] = tokens[mul_div_idx] = tokens[mul_div_idx+1] = None
-#         stack.append({
-#                 'operator': op,
-#                 'left': numB4,
-#                 'right': numafter
-#             })
-#         try:
-#             mul_div_idx = next((i for i, x in enumerate(tokens) if x in ['*', '/']), -1)
-#         except ValueError:
-#             break
-    
-#     # Handle addition and subtraction
-#     add_sub_idx = next((i for i, x in enumerate(tokens[::-1]) if x in ['+', '-']), -1)
-#     while add_sub_idx > -1:
-#         numB4 = tokens[add_sub_idx-1]
-#         numafter = tokens[add_sub_idx+1]
-#         op  = tokens[add_sub_idx]
-#         tokens[add_sub_idx-1] = tokens[add_sub_idx] = tokens[add_sub_idx+1] = None
-#         stack.append({
-#                 'operator': op,
-#                 'left': numB4,
-#                 'right': numafter
-#             })
-#         try:
-#             add_sub_idx = next((i for i, x in enumerate(tokens[::-1]) if x in ['+', '-']), -1)
-#         except ValueError:
-#             break
-    
-#     if len(stack) != 1:
-#         raise ValueError("Invalid expression")
-#     return stack.pop()
-
-
-# def print_tree(node, level=0):
-#     if isinstance(node, dict):
-#         print_tree(node['right'], level + 1)
-#         print(' ' * 4 * level + '-> ' + node['operator'])
-#         print_tree(node['left'], level + 1)
-#     else:
-#         print(' ' * 4 * level + '-> ' + str(node))
-
-# def evaluate_tree(node):
-#     if isinstance(node, dict):
-#         left_value = evaluate_tree(node['left'])
-#         right_value = evaluate_tree(node['right'])
-#         operator = node['operator']
-        
-#         if operator == '+':
-#             return left_value + right_value
-#         elif operator == '-':
-#             return left_value - right_value
-#         elif operator == '*':
-#             return left_value * right_value
-#         elif operator == '/':
-#             return left_value / right_value
-#         elif operator == '^':
-#             return left_value ** right_value
-#         else:
-#             raise ValueError(f"Invalid operator: {operator}")
-#     else:
-#         return node
-
-
-def calculate_expression(expression):
-    
-    expression_tree = create_tree(expression)
-    print_tree(expression_tree)
-    result = evaluate_tree(expression_tree)
-    return result
 
 
 app = FastAPI()
+register_default_operators()
 
-@app.post("/calculate")
-async def perform_calculation(calculation: Calculation):
-    result = calculate(calculation.operator, calculation.operand1, calculation.operand2)
-    return {"result": result}
+
+origins = ["*"]  # Replace with the allowed origins
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+    
 
 @app.post("/calculate_str")
-async def perform_calculation(calculation: CalculationString):
-    result = calculate_expression(calculation.expression)
-    return {"result": result}
+async def perform_calculation_str(calculation: CalculationString, style: bool = False):
+    try:
+        result = compute(calculation.expression)
+        if style:
+            if result % 2 == 0:
+                color = "green"
+            else:
+                color = "red"
+            return {"result": result, "color": color}
+        else:
+            return {"result": result}
+    except KeyError as key:
+        raise ValueError(f"Symbol {key} unknown in {CalculationString}")   
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", port=8001, log_level="debug", reload=True)
--- test_api.py
+++ test_api.py
@@ -4,26 +4,11 @@ from  app import app
 
 client = TestClient(app)
 
-@pytest.mark.parametrize("operand1, operator, operand2, expected", [
-    (3, "+", 4, 7),
-    (10, "-", 6, 4),
-    (2, "*", 5, 10),
-    (10, "/", 2, 5),
-    (2, "^", 3, 8),
-])
-def test_calculate_endpoint(operand1, operator, operand2, expected):
-    response = client.post("/calculate", json={
-        "operand1": operand1,
-        "operator": operator,
-        "operand2": operand2
-    })
-    assert response.status_code == 200
-    assert response.json()["result"] == expected
-
 @pytest.mark.parametrize("expression, expected", [
-    ("0.01 + 0.99+ 2 * 3 - 4 / 2 + 2 ^ 3", 13),
+    ("2.0 ^ 3.0", 8.0),
+    ("0.99+0.01+ 2.0 * 3.0 - 4.0 / 2.0 + 2.0 ^ 3.0", 13.0),
     ("(3 + 4) - 2 * 5 ", -3),
-    ("10 - 3 + 2 * 4 / 2", 11),
+    ("0.99+0.01+ 2.0 * 3.0 - 4.0",3)
 ])
 def test_calculate_string_endpoint(expression, expected):
     response = client.post("/calculate_str", json={
@@ -32,20 +17,27 @@ def test_calculate_string_endpoint(expression, expected):
     assert response.status_code == 200
     assert response.json()["result"] == expected
 
-def test_invalid_operator():
-    response = client.post("/calculate", json={
-        "operand1": 5,
-        "operator": "%",
-        "operand2": 3
-    })
-    assert response.status_code == 422
-    assert response.json()["detail"][0]["msg"] == "Invalid operator"
 
 def test_division_by_zero():
-    response = client.post("/calculate", json={
-        "operand1": 10,
-        "operator": "/",
-        "operand2": 0
-    })
+    try:
+        response = client.post("/calculate_str", json={
+            "expression": "10/0"
+        })
+
+    except ZeroDivisionError:
+        pass
+
+ 
+@pytest.mark.parametrize("expression, expected", [
+    ("2.0 ^ 3.0", "blue"),
+    ("1*13", "red"),
+    
+])
+def test_calculate_style(expression, expected):
+    response = client.post("/calculate_str", json={
+        "expression": expression
+    }, params={"style":"true"})
+    
     assert response.status_code == 200
-    assert response.json()["result"] == "Error: Division by zero"
+    assert response.json()["color"] == expected
+   
